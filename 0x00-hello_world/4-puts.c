8f9e0af9abe071812765582bf6caf811bf661b9a
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-17 01:15:00 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return - Always 0 (success)
+ *
+ */
+
+int main ()
+	{
+	
+		push("\"Programming is like building a multilingual puzzle\n")
+
+			return 0;
+	
+	}
