b8605a0d3349d1a2e4fab012f955b02838811343
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-19 05:13:33 -0800

    6-size.c added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,13 @@
  */
 int main(void)
 {
-	printf("The size of int: %d bytes\n", sizeof(int));
-	printf("The size of char: %d bytes\n", sizeof(char));
-	printf("The size of float: %d byts\n", sizeof(float));
-	printf("The size of long: %d bytes\n", sizeof(long));
+	printf("The size of int: %lu bytes(s)\n", sizeof(int));
+	printf("The size of char: %lu bytes(s)\n", sizeof(char));
+	printf("The size of float: %lu byts(s)\n", sizeof(float));
+	printf("The size of long: %lu bytes(s)\n", sizeof(long));
+	printf("The size of short: %lu bytes(s)\n", sizeof(short));
+	printf("The size of long int: %lu bytes(s)\n", sizeof(long int));
+	printf("The size of unsigned short: %lu bytes(s)\n", sizeof(unsigned long));
+	printf("The size of unsigned long: %lu bytes(s)\n", sizeof(unsigned short));
 	return (0);
 }
