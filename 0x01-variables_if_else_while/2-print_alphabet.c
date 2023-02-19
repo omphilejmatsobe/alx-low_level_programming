e07268660ccd1261c6c020d128af9bebf4e0e32e
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-19 10:23:22 -0800

    2-print_alphabet file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		putchar(cha);
+	}
+	putchar('\n');
+	return (0);
+}
