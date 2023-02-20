52b390b305e8a9f9b7a2ef625bed50573a38c1f9
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-20 06:50:37 -0800

    8-print_base16.c file added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char hex;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
