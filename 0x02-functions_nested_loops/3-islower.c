c09b33e20907859e42e2acf4a31a6f16f554c7c5
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-22 13:34:36 -0800

    3-islower file update 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - checks if the code works
+ */
+int _islower(int c)
+{
+	char cah;
+
+	if (cah >= 'a' && cah <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
