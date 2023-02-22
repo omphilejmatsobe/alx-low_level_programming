2e80888c0910f97e33a97afd788aa057aa2a7d66
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-22 15:55:15 -0800

    101-natural.c added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - lists all the natural numbers below 1024 (excluded)
+ *
+ * Return: Always 0.
+*/
+	int main(void)
+	{
+		int i, sum = 0;
+
+
+		for (i = 0; i < 1024; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
+				sum += i;
+		}
+
+
+		printf("%d\n", sum);
+
+
+		return (0);
+	}
+
+
