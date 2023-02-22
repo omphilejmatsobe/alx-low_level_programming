2f97012a6704bbf5136d92e8850eb1c5a25bdc3a
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-22 10:41:16 -0800

    main.h file updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,8 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return:Always 0 (success)
- */
-int main(void)
-{
-	return (0);
-}
+#ifndef main.h
+#define main.h
+
+int_putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
