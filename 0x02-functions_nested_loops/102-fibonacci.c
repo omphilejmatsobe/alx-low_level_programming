a49a460dc7761c258c58dfce32a1e9d6a5718f84
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-22 16:16:05 -0800

    102-fibonacci file update 1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,8 +3,7 @@
 
 /**
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- *       
- *
+ * 
  * Return: Always 0.
  */
 int main(void)
@@ -12,7 +11,7 @@ int main(void)
 	int num;
 	unsigned long fibu1 = 0, fibu2 = 1, sum;
 
-	for (num = 0; num <=49; num++)
+	for (num = 0; num <= 49; num++)
 	{
 		sum = fibu1 + fibu2;
 		printf("%lu", sum);
