f4fbecee0bd4371a38c7d6b2b2fd50fe6e62d95d
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-22 17:07:14 -0800

    Check if isalpha works

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,15 @@
 /**
  * _isalpha - checks if character is alphabet
  * Returns: 1 if alphabet otherwise 0
- */
+ *@c: is the integer value for the characters
+*/
 int _isalpha(int c)
 {
 	if ( c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else if ( c >= 'A' && C <= 'Z')
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
