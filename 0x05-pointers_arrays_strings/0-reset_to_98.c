b8e3b1aa9db33c8cdd703393e496190913933c2f
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-02-27 11:15:01 -0800

    Pointers_Arrays_Strings projects uploaded

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ *
+ * Return: Always 0 (success)
+ */
+void reset_to_98(int *p);
+{
+	*p = 98
+        return *p;
+}
