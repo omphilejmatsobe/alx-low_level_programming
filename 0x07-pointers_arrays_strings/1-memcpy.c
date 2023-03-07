9085e5d17b157a51d2b66ac044c3de9b281e0de7
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-03-07 05:32:28 -0800

    All files updated

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int x;
+	int x;
+	int y = n;
 
-	for (x = 0; x < n; x++)
+	for (x = 0; x < y; x++)
 	{
 		dest[x] = src[x];
 		n--;
 	}
 	return (dest);
 }
-
