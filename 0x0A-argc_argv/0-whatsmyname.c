7f6c366316e08ab0bd5262c40d23ed8a0f638042
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-03-10 10:54:35 -0800

    Files updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *main - prints name of the program
+ *@argc: argumnet controll
+ *@argv: argument vector
  *
- *
+ *return : Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	if ( argc >= 0)
-		printf("%s\n",argv[0]);
+	if (argc >= 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
-
