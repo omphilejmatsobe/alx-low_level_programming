aa1398a7fcfe3a2d096d58609ca5476e853d8cf7
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-03-14 00:15:48 -0700

    Create array file update 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	cha = malloc(size * sizeof(char));
 
 	if (cha == NULL || cha == 0)
-                return (NULL);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		cha[i] = c;
