038da4eda3d1247e79eab7d3c1650b016ad15e6c
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-03-21 02:13:33 -0700

    dog.h file update 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,18 @@
+#define dog_h
 #include "main.h"
+
+/**
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member 
+*
+* Description: Longer description
+*/
+
 struct dog
 {
-	char name;
+	char *name;
 	float age;
-	char owner;
+	char *owner;
 }
