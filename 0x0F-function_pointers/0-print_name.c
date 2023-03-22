89bac7cfb65afd2c0bce16ad2954e1ca10c2dddb
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-03-22 10:35:56 -0700

    print name files added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,8 @@
 /**
  * print_name - print a name
  *
- * @name - pointer to function that prints the name
- * @f - pointers to function
+ * @name: pointer to function that prints the name
+ * @f: pointers to function
  */
 void print_name(char *name, void (*f)(char *))
 {
