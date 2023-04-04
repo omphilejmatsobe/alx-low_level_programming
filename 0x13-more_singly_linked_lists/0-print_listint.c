17d0a89c43635a8bea8f50ac96c14cc5d2b51041
Omphile Matsobe <omphilejmatsobe@gmail.com>
2023-04-04 02:05:01 -0700

    dependency files added

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * size_t print_listint - prints all the elements of a listint_t list
+ * @h - pointer
+ *
+ * Return - size of int
+ */
+size_t print_listint(const listint_t *h)
+{
+
+}
