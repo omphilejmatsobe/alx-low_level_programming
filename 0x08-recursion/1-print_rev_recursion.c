51daf95ad58b89a84e9336d521d4e20f0c186a8f
Omphile <omphilejmatsobe@gmai.com>
2023-06-07 14:57:43 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *_print_rev_recursion - prints strings in reverse order
+ *_print_rev_recursion - prints a string in reverse
  *
- *@s: input pointer to string
+ * @s: input pointer to string
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		s = s + 1;
-		_print_rev_recursion(s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
+
