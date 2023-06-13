1fa2d89913c24f4a5084a09c3c21c893c0a01dde
Omphile <omphilejmatsobe@gmai.com>
2023-06-13 13:15:08 -0700

    0-create_array file added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL || size == 0)
-                return (NULL);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
