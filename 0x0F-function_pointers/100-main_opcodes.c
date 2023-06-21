d5f382e06122e4b01cb57d665e7c371f0a24128d
Omphile <omphilejmatsobe@gmai.com>
2023-06-21 16:58:38 -0700

    file updated

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,7 +3,7 @@
 /**
  * main - prints opcode of the function
  * @argc: arg count
- * @argv: input argument 
+ * @argv: input argument
  * Return: Exit 1 if one arg is not correct, 2 if byte is negative.
  */
 int main(int argc, char *argv[])
@@ -15,13 +15,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+
 	i = atoi(argv[1]);
+
 	if (i < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+
 	printf("%02x", *((unsigned char *) (main)));
+
 	for (y = 1; y < i; ++y)
 		printf(" %02x", *((unsigned char *) (main + y)));
 	putchar('\n');
