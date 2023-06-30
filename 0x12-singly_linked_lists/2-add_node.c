520857d7f7dd7a29544410b7a6e12573aa2b3b81
Omphile <omphilejmatsobe@gmai.com>
2023-06-30 14:59:35 -0700

    fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 
 /**
  ** add_node - Adds a new node to a list
- * @head: Head 
+ * @head: Head
  * @str: The new value for the node
  *
  * Return: Address
@@ -11,30 +11,30 @@ list_t *add_node(list_t **head, const char *str)
 {
 	int x;
 	list_t *new;
-	
+
 	if (str == NULL || head == NULL)
 		return (NULL);
-	
+
 	new = malloc(sizeof(list_t));
-	
+
 	if (new == NULL)
 		return (NULL);
-	
+
 	for (x = 0; str[x] != 0; x++)
 		;
-	
+
 	new->str = malloc(x + 1);
-	
+
 	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	
+
 	new->len = x;
 	new->str = strdup(str);
 	new->next = *head;
 	*head = new;
-	
+
 	return (new);
 }
