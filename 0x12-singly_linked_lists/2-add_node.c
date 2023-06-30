7eca7fe31d07de2aa000510eff5235fa4ff2f251
Omphile <omphilejmatsobe@gmai.com>
2023-06-30 14:56:26 -0700

    list updated

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ ** add_node - Adds a new node to a list
+ * @head: Head 
+ * @str: The new value for the node
+ *
+ * Return: Address
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+	int x;
+	list_t *new;
+	
+	if (str == NULL || head == NULL)
+		return (NULL);
+	
+	new = malloc(sizeof(list_t));
+	
+	if (new == NULL)
+		return (NULL);
+	
+	for (x = 0; str[x] != 0; x++)
+		;
+	
+	new->str = malloc(x + 1);
+	
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	
+	new->len = x;
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+	
+	return (new);
+}
