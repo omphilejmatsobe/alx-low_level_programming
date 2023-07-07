c7b83f5755165c6986d8cd491d50e3d1d1859aef
Omphile <omphilejmatsobe@gmai.com>
2023-07-07 14:15:05 -0700

    File added

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - set the value to 1 at a given index
+ * Return: 1 if little endian 0
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+
+	char *b = (char *)&x;
+	if (*b)
+		return (1);
+	return (0);
+}
