dcc48656853a0d016493bcca1f2a8a54444a72d4
Omphile <omphilejmatsobe@gmai.com>
2023-07-09 00:29:19 -0700

    print_most-numbers files added

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 - 9 with exceptions
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			 printf("%d", i);
+	}
+	putchar('\n');	
+}
