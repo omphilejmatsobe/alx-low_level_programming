b23a2d6e63e097b51b3e23e431ea8791a69c6c37
Omphile <omphilejmatsobe@gmai.com>
2023-07-09 01:38:04 -0700

    diagonal lines added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - draws diagonal line on terminal
+ * @n: input number of spaces before line
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i != 0)
+			{
+				for (j = 0; j < i; j++)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	
+	else
+	{
+		_putchar('\n');
+	}
+}
