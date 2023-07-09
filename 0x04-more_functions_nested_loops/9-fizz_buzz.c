2a2b183559e30dbee31ead9bc0efb4a7f36a6872
Omphile <omphilejmatsobe@gmai.com>
2023-07-09 03:18:17 -0700

    fizz buzz task added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) != 0)
+			printf("Fizz");
+		else if ((i % 3) != 0 && (i % 5) == 0)
+			printf("Buzz");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+
+		putchar('\n');
+	}
+	return(0);
+}
