1efe1274dceec3e9cfe42e956d502aa0f34b925e
Omphile <omphilejmatsobe@gmai.com>
2023-07-09 05:47:53 -0700

    triagle files added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle followed by a new line
+ *
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int space, hash, x, y;
+
+	x = 0;
+
+	if (size > 0)
+	{
+		for (y = 0; y < size; y++)
+		{
+			x++;
+
+			for (space = (size - x); space > 0; space--)
+			{
+				_putchar(' ');
+			}
+
+			for (hash = 1; hash <= x; hash++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+			
+		}
+	}
+	else
+		_putchar('\n');
+}
