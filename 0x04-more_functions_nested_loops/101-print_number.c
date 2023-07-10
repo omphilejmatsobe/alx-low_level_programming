48f550df3206ec8863bad0736101a19314292d7d
Omphile <omphilejmatsobe@gmai.com>
2023-07-10 00:19:14 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,26 @@
 #include "main.h"
-#include <math.h>
-#include <stdlib.h>
 /**
- * print_number - prints int with putchar
- * @n: int to be printed
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	int x, num, _int, pwr, i;
-	int c, ctr = 0;
-
-	c = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		n = n * (-1);
+		n1 = -n;
 		_putchar('-');
 	}
-
-	if (c == 0)
+	else
 	{
-		ctr = 1;
-	}
-	while (c != 0)
-	{
-		c = c / 10;
-		ctr++;
+		n1 = n;
 	}
 
-
-	for (x = 0; x < ctr; x++)
+	if (n1 / 10)
 	{
-		pwr = 1;
-		for (i = 1; i < ctr - x; i++)
-		{
-			pwr = pwr * 10;
-		}
-
-		num = n % pwr;
-		_int = (n - num) / pwr;
-		n = num;
-
-		_putchar(48 + _int);
+		print_number(n1 / 10);
 	}
+
+	_putchar((n1 % 10) + '0');
 }
