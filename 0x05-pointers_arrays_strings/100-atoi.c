674fcf269ed3a61885f6022e082b22160ceb60a7
Omphile <omphilejmatsobe@gmai.com>
2023-07-14 14:10:27 -0700

    file test 100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,26 +10,33 @@
 int _atoi(char *s)
 {
 	int sign, out, pow, i, firstDig, digCount, x, y;
-	bool hasDigits, isDigit;
+	bool hasDigits, isDigit, makeBreak;
 
-	hasDigits = isDigit = false;
+	hasDigits = isDigit = makeBreak = false;
 	pow = 1;
 	digCount = out = i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 48 && s[i] <= 57)
+		if ((s[i] >= 48 && s[i] <= 57))
 		{
 			hasDigits = isDigit = true;
 			digCount++;
 			firstDig = i - digCount + 1;
-			if (s[firstDig - 1] == 45)
+			if (s[firstDig - 1] == 45 && (s[firstDig - 2] == 20 || s[firstDig - 2] == 0 ))
 				sign = (-1);
 			else
 				sign = 1;
+
+			if ((s[i + 1] < 48) || (s[i + 1] > 57))
+				makeBreak = true;
+
 		}
 		else
 			isDigit = false;
 		i++;
+
+		if (makeBreak == true)
+			break;
 	}
 
 	if (hasDigits == true)
