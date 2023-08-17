41ef6e6be1b009cffebac792494aa2e551559943
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-17 23:16:57 +0300

    file test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	for (; idx != 1; idx--)
 	{
-		tmo = tmp->next;
+		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
 
