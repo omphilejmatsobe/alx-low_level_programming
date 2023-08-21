377d994d7d985fd3461e1826d5acba0d9441f23c
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-21 22:00:01 +0300

    100-operations.so file added

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,50 @@
+/**
+ * add - sum of a and b
+ * @a: operand 1
+ * @b: operand 2
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substract of a and b
+ * @a: operand 1
+ * @b: operand 2
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply of a and b
+ * @a: operand 1
+ * @b: operand 2
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divide of a and b
+ * @a: operand 1
+ * @b: operand 2
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - module of a and b
+ * @a: operand 1
+ * @b: operand 2
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
