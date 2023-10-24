382910f9224a7b791db013ead1fbc81050a544df
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-25 00:00:31 +0300

    Alloc Grid function created

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,6 +23,9 @@ int **alloc_grid(int width, int height)
 	{
 		arr[x] = malloc(sizeof(int) * width);
 
+		if(arr[x] == NULL)
+			return (NULL);
+
 		for (y = 0; y < width; y++)
 			arr[x][y] = 0;
 	}
