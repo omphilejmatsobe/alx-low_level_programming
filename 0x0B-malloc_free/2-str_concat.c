7ca1f257690751b33f9726cd439734d98b417743
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-24 20:12:49 +0300

    String Concat function created.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+/**
+ * str_concat - contains the contents of two strings
+ * @s1: first input string
+ * @s2: second inpit string
+ *
+ * Return: pointer to newly allocated space in memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	unsigned int x, y, z;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	x = strlen(s1);
+	y = strlen(s2);
+
+	arr = malloc(sizeof(char) * (x + y));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (z = 0; z < x; z++)
+		arr[z] = s1[z];
+
+	for (z = 0; z < y; z++)
+		arr[z + x] = s2[z];
+
+	return (arr);
+}
