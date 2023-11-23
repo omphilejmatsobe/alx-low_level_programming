8a9bce594da8852bf524ec9b645bdcece6a496fe
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-23 18:00:08 +0200

    str-concat function implimented. File test complete.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,10 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	unsigned int x;
+	unsigned int x, sizeOne, sizeTwo;
+
+	sizeOne = strlen(s1);
+	sizeTwo = strlen(s2);
 
 	x = 0;
 
@@ -18,30 +21,33 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
+	else if (s1 == NULL)
+	{
+		sizeOne = 0;
+	}
+	else if (s2 == NULL)
+	{
+		sizeTwo = 0;
+	}
 
-	arr = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+
+	arr = malloc((sizeOne + sizeTwo + 1) * sizeof(char));
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (x = 0; x < strlen(s1); x++)
+	for (x = 0; x < sizeOne; x++)
 	{
-		arr[x] = s1[x]; 
+		arr[x] = s1[x];
 	}
 
-	for (x = 0; x < strlen(s2); x++)
+	for (x = 0; x < sizeTwo; x++)
 	{
-		arr[strlen(s1)+ x] = s2[x];
+		arr[strlen(s1) + x] = s2[x];
 	}
 
-	if (arr == NULL)
-	{
-		free(NULL);
-		return (NULL);
-	}
-
-	arr[strlen(s1) + strlen(s2)] = '\0';
+	arr[sizeOne + sizeTwo] = '\0';
 
 	return (arr);
 }
