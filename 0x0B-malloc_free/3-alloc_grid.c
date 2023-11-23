5d4dff2a2b1a036f3d292f4ecfd32c29f85f9b08
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-23 18:52:18 +0200

    alloc_grid function complete. File test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	arr = malloc((height * sizeof(int *)) + 1);
+	arr = malloc(height * sizeof(int *) + 1);
 	if (arr == NULL)
 	{
 		free(arr);
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (x = 0; x < height; x++)
 	{
-		arr[x] = malloc((width * sizeof(int)) + 1);
+		arr[x] = malloc(width * sizeof(int) + 1);
 		if (arr[x] == NULL)
 		{
 			while (x > 0)
