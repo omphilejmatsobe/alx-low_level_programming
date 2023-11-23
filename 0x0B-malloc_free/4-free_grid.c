0a84c5ed1a060e2cfe3d883e0e3a144eaf7ceafa
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-23 19:33:25 +0200

    free_grid function complete. File test

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,10 +8,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	int x;
+	while (height >= 0)
+	{
+	       free(grid[height]);
+	       height++;
+	}
 
-	for (x = 0; x < height; x++)
-	       free(grid[x]);
-
-	free(grid);	
+	free(grid);
 }
