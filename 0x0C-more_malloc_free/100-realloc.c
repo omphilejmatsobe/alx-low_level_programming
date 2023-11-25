52f735601cf9772f39bacf9d1a0a80f0234c0eef
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-25 22:54:27 +0200

    File test

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,13 +16,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	if (old_size == new_size)
+	{
+		free (pointer);
 		return (ptr);
+	}
 
 	if (ptr == NULL)
 		return (NULL);
 
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-	copy = (char *) ptr;
+	copy =  ptr;
 
 	for (x = 0; x < sizeof(ptr); x++)
 	{
