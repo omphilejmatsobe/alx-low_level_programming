1bc88835392fd0916ef651b440be516e4032f208
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-25 23:13:31 +0200

    File check

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,21 +11,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *pointer, *copy;
 	unsigned int x;
 
-	pointer = malloc(new_size);
-	if(pointer == NULL)
-		return (NULL);
-
 	if (old_size == new_size)
-	{
-		free (pointer);
 		return (ptr);
-	}
 
 	if (ptr == NULL)
-	{
-		free(pointer);
 		return (NULL);
-	}
 
 	if (new_size == 0)
 	{
@@ -33,12 +23,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	pointer = malloc(new_size);
+	if (pointer == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	copy =  ptr;
 
-	for (x = 0; x < sizeof(ptr); x++)
+	for (x = 0; x < old_size; x++)
 	{
 		pointer[x] = copy[x];
 	}
+
 
 	free(ptr);
 	return (pointer);
