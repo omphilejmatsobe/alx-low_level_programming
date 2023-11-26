cbb84130bcc1eb2f395193fcf64c9e3754a33b84
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-26 02:58:51 +0200

    File test

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc , char **argv)
+{
+	long unsigned int x, y;
+
+	if (argc > 3)
+		exit(98);
+
+	for (x = 1; x < (long unsigned int) argc; x++)
+	{
+		for (y = 0; y < sizeof(argv[x]); y++)
+		{
+			if (argv[x][y] > 57 && argv[x][y] < 48)
+			{
+				exit(98);
+			}
+		}
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
