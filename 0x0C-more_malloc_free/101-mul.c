ec2471a32ba7797dab8f262b8eb2dda3096b88a2
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-27 09:26:45 +0200

    File test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * main - prints all arguments received.
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: if not receive 2 arg, rt error
  */
-int main(int argc , char **argv)
+int main(int argc, char *argv[])
 {
-	long int x, y, prod;
+	int n1, n2, mul;
 
-	if (argc > 3)
+	if (argc != 3)
+		printf("Error\n");
+	else
 	{
-		printf("Error");
-		exit(98);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		mul = n1 * n2;
+		printf("%d\n", mul);
 	}
-	for (x = 1; x < (long int) argc; x++)
-	{
-		for (y = 0; y < (long int) sizeof(argv[x]); y++)
-		{
-			if (argv[x][y] > 57 && argv[x][y] < 48)
-			{
-				printf("Error");
-				exit(98);
-			}
-		}
-	}
-
-	prod = x * y;
-
-	printf("%li\n", prod);
 	return (0);
 }
+
