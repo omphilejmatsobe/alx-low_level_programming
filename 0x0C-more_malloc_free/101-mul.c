9d8db0a30ad2d8eaac601d709f335df7d5d1e3e3
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-27 09:25:15 +0200

    File test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,16 +8,16 @@
  */
 int main(int argc , char **argv)
 {
-	long unsigned int x, y;
+	long int x, y, prod;
 
 	if (argc > 3)
 	{
 		printf("Error");
 		exit(98);
 	}
-	for (x = 1; x < (long unsigned int) argc; x++)
+	for (x = 1; x < (long int) argc; x++)
 	{
-		for (y = 0; y < sizeof(argv[x]); y++)
+		for (y = 0; y < (long int) sizeof(argv[x]); y++)
 		{
 			if (argv[x][y] > 57 && argv[x][y] < 48)
 			{
@@ -27,6 +27,8 @@ int main(int argc , char **argv)
 		}
 	}
 
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	prod = x * y;
+
+	printf("%li\n", prod);
 	return (0);
 }
