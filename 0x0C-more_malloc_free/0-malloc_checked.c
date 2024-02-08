fb1167c2cf737c07eb1635eeed05853c736b7439
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-08 07:51:34 +0200

    Memory Allocation function works.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory to a variable.
+ * @b: The input variable to be allcocated memory. 
+ *
+ * Return: Pointer to the newly allocated memory.
+ * If the program fails it returns Null.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *new_memory;
+
+	new_memory = malloc(b);
+	if (new_memory == NULL)
+	{
+		free(new_memory);
+		exit(98);
+	}
+
+	return (new_memory);
+}
