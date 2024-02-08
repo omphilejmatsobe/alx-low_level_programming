974adaac48b0db13b4d881b3b173de6d4b658487
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-08 08:57:48 +0200

    String concatenation function complete. Tests in progress.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= strlen(s2))
 		n = strlen(s2);
 
-	concat = malloc(sizeof(concat) * (strlen(s1) + n) + 1);
+	concat = malloc((sizeof(*concat) * (strlen(s1) + n)) + 1);
 	if (concat == NULL)
 	{
 		free(concat);
