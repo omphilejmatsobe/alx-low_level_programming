2feb486f50ca6caebd9881501f409254713ab249
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-11 15:28:23 +0200

    Free function is complete

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * d - Input strct dog_t to be freed.
+ *
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
