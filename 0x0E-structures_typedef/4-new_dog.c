71fffc15713acfa80938a9de71e049502e9fb410
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-11 15:08:46 +0200

    New instance of dog_t creates. Function new_dog complete.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include "string.h"
 
 /**
+ * _strcpy - copies the string pointed to by src
+ *
+ * @dest: the copying string
+ * @src: the string being copied
+ * Return: the copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int i = 0;
+
+        while (*src != '\0')
+        {
+                dest[i] = *src;
+                src++;
+                i++;
+        }
+
+        if (*src == '\0')
+        {
+                dest[i] = '\0';
+        }
+
+        return (dest);
+}
+
+
+/**
  * new_dog - Creates a new instance dog of struct dog_t.
  * @name: Input string to be saved in variable name of dog.
  * @age: Input float to be saved in variable age of dog.
