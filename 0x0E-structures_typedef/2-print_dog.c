40d2f727a709db8c1fdcf7928cc9f7987f776fda
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-11 13:06:07 +0200

    print_dog function complete. Fixes complete..

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,13 +13,13 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: %p\n", (void *) d->name);
 		else
-			printf("Name: %s\n",d->name);
+			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
 		if (d->name == NULL)
 			printf("Owner: %p\n", (void *) d->owner);
 		else
-			printf("Owner: %s\n",d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
