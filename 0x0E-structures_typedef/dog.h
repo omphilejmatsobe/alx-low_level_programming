57ba3324c8930b733332f3d90b48140544218a1c
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-11 12:16:53 +0200

    Dog structure written, fixes complete.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef _DOG_
 #define _DOG_
 
-
+/**
+ * struct dog - Entities of a dog.
+ * @name: The name of the dog.
+ * @owner: The name of the owner of the dog.
+ * @age: The age of the dog.
+ *
+ * Description: This is a structure to store information about a dog.
+ */
 struct dog
 {
 	char *name;
