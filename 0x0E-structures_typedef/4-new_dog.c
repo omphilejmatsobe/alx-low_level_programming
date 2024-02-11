dbbf473924e28b30b6b47ef8d22cc6d3a74d73de
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-02-11 15:16:20 +0200

    New instance of dog_t creates. Function new_dog complete.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,21 +10,21 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-        int i = 0;
+	int i = 0;
 
-        while (*src != '\0')
-        {
-                dest[i] = *src;
-                src++;
-                i++;
-        }
+	while (*src != '\0')
+	{
+		dest[i] = *src;
+		src++;
+		i++;
+	}
 
-        if (*src == '\0')
-        {
-                dest[i] = '\0';
-        }
+	if (*src == '\0')
+	{
+		dest[i] = '\0';
+	}
 
-        return (dest);
+	return (dest);
 }
 
 
