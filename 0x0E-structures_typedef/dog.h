97e29e83930c0ce71ce471070317a21a072d243c
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-28 18:59:47 +0200

    New name for the structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define _DOG_H_
 
 /**
- * struct dog - struct that stores dog object and its attributes
- * @name: name of dog
- * @age: number of years the dog lived
- * @owner: the owner of the dog
+ * struct dog - Struct that stores dog object and its attributes.
+ * @name: Name of dog.
+ * @age: Number of years the dog lived.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
@@ -13,6 +13,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - New name for struct dog.
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
