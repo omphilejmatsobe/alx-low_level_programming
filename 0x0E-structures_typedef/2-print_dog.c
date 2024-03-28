e3a1614e30cdbd5c1bf7f9cc63ea266124fe3d5f
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-28 18:54:09 +0200

    Function to print elements of structure complete

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,9 +3,9 @@
 #include "dog.h"
 
 /**
- * print_dog - prints a struct dog.
- * @d: struct to be printed.
- * 
+ * print_dog - Prints a struct dog.
+ * @d: Input struct to be printed.
+ *
  * Return: None.
  */
 void print_dog(struct dog *d)
@@ -13,14 +13,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: Nil\n");
+			printf("Name: %p\n", (void *) d->name);
 		else
 			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
-		
+
 		if (d->name == NULL)
-			printf("Owner: Nil\n");
+			printf("Owner: %p\n", (void *) d->owner);
 		else
 			printf("Owner: %s\n", d->owner);
 	}
