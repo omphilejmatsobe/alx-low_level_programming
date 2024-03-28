c502738e715032973f2936f20907ba9d94d2a7c6
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-28 20:02:45 +0200

    Function to create the new instance of the struct works. Frees everthing when it fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,15 +15,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return (NULL);
+	{
+		free(name);
+		free(owner);
+		free(dog);
 
+		return (NULL);
+	}
 	dog->name = malloc(strlen(name) + 1);
 	if (dog->name == NULL)
 	{
 		free(dog->name);
 		free(name);
 		free(owner);
-		free(dog->owner);
 		free(dog);
 		return (NULL);
 	}
