ea227cd5d6de92f43a3087ae67c3d4d1e9ceef72
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-28 21:16:28 +0200

    Function to create the new instance of the struct works. Frees everthing when it fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,35 @@
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- *
- *
- *
- *
- *
+ * new_dog - entry point
+ * @name: string from main, name of pet
+ * @age: number from main, age of pet
+ * @owner: string from main, owner of pet
+ * Return: p
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
 		return (NULL);
-
-	if (name == NULL || owner == NULL)
+	/* Cpunting name pointer*/
+	if (name == NULL)
 	{
-		free(name);
+		free(p);
 		free(owner);
 		return (NULL);
 	}
-
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-
-	return (dog);
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
