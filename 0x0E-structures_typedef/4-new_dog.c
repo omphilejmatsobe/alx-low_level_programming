c275ec25eb4790736080718989fcfed7e2d5a4f4
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-28 21:14:28 +0200

    Function to create the new instance of the struct works. Frees everthing when it fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,10 @@
 #include "dog.h"
-#include <string.h>
-
 /**
- * new_dog - Creates a new instance of the struct dog_t.
- * @name: Input string to be passed to the new instance of dog_t.
- * @age: Input float to be passed to the new instance of dog_t.
- * @owner: Input string to be passed to the new instance of dog_t.
  *
- * Return: If success, new instance of dog_t.
+ *
+ *
+ *
+ *
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -15,53 +12,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+		return (NULL);
+
+	if (name == NULL || owner == NULL)
 	{
-		free(dog);
+		free(name);
+		free(owner);
 		return (NULL);
 	}
 
-	dog->name = malloc(strlen(name) + 1);
-	if (dog->name == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	_strncpy(dog->name, name, strlen(name));
-
+	dog->name = name;
 	dog->age = age;
-
-	dog->owner = malloc(strlen(owner) + 1);
-	if (owner == NULL)
-	{
-		free(dog->name);
-		free(dog->owner);
-		free(dog);
-		return (NULL);
-	}
-	_strncpy(dog->owner, owner, strlen(owner));
+	dog->owner = owner;
 
 	return (dog);
-}
-
-/**
- * _strncpy - Copies a string.
- * @dest: Input destination of the copy.
- * @src: Input string being copied.
- * @n: Input number of max bytes.
- *
- * Return: the copied string.
- */
-char *_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int x;
-
-	for (x = 0; x < n; x++)
-	{
-		dest[x] = src[x];
-	}
-
-	dest[x] = '\0';
-
-	return (dest);
 }
