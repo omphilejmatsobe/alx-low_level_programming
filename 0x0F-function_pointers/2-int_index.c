6f942c55c71e7faa8da4ce08de1ed18032e2df3c
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-29 16:19:37 +0200

    FIle check

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int counter;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL || size <= 0 || (*cmp) == NULL)
 		return (-1);
 
 	for (counter = 0; counter < size; counter++)
