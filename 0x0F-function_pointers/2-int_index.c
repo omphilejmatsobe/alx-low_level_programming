4fbb18097bc6c19701a76268c63b8449f399cbdf
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-29 18:25:09 +0200

    int_index function completed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,7 +2,7 @@
 /**
  * int_index - Searches for an integer.
  * @array: Input array.
- * @size: The size of the array. 
+ * @size: The size of the array.
  * @cmp: The pointer to the fuction that check if index of array is an int.
  *
  * Return: Index of array, if fails return -1;
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (counter = 0; counter < size; counter++)
 	{
 		if ((*cmp)(array[counter]) > 0)
-			return(counter);
+			return (counter);
 	}
 
 	return (-1);
