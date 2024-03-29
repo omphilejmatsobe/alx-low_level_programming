a24d60edc71be6294b0950415f2d22b034e483e9
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-29 16:17:20 +0200

    FIle check

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer.
+ * @array: Input array.
+ * @size: The size of the array. 
+ * @cmp: The pointer to the fuction that check if index of array is an int.
+ *
+ * Return: Index of array, if fails return -1;
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int counter;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	for (counter = 0; counter < size; counter++)
+	{
+		if ((*cmp)(array[counter]) != 0)
+			return(counter);
+	}
+
+	return (0);
+}
