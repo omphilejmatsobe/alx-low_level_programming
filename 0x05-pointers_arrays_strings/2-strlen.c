5dba3801cd051fa0500af4e6c01e9e63faff34c8
omphilejmatsobe <omphilejmatsobe@gmail.com>
2025-01-27 18:01:41 +0200

    Task update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,15 @@
  * _strlen - Returns the length of a string
  *
  * @s: The string paramenter
- * Returns: Length of the string
+ * Return: Length of the string
  */
 int _strlen(char *s)
 {
 	int length = 0;
 	int i;
 
-	for (i = 0; *(s + i) != '\0'; i++) 
+	for (i = 0; *(s + i) != '\0'; i++)
 		length++;
 
-	return length;
+	return (length);
 }
