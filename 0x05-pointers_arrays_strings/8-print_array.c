cccb2e54ce3b9b4b5a710f01fc010851d60704ab
omphilejmatsobe <omphilejmatsobe@gmail.com>
2025-01-30 10:59:12 +0200

    Print array added

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,17 +10,13 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && *a != '\n')
+	for (i = 0;i < n && *(a + i) != '\n'; i++)
 	{
 		printf("%d", *a);
 		if (i < n - 1)
-		{
 			printf(", ");
-		}
-		a++;
-		i++;
 	}
 
 	printf("\n");
