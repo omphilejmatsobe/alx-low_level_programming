99764b1a6de855a0309e295e9001701278f76c56
omphilejmatsobe <omphilejmatsobe@gmail.com>
2025-02-04 14:46:18 +0200

    app update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,19 @@
 #include "main.h"
-/**
- *_memset - fills memory with a constant byte
- *@s: memory where is stored
- *@b: memory where is copied
- *@n: number of bytes
+
+/*
  *
- *Return: copied memory with n byted changed
+ *
+ *
+ *
+ *
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	int x;
+	unsigned int idx;
 
-	for (x = 0; n > 0; x++)
-	{
-		s[x] = b;
-		n--;
-	}
-	return (s);
+	for(idx = 0; idx < n; idx++)
+		*(s + idx) = b;
+
+	return s;
 }
